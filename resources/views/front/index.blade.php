51d0b1ff4e9317c25e82de5dc34ecb803549a666
Bagas Miftahun Na'im <bagasmiftahunaim@gmail.com>
2024-08-07 15:04:14 +0700

    fix : reusable Navbar Component

--- resources/views/front/index.blade.php
+++ resources/views/front/index.blade.php
@@ -5,35 +5,7 @@
     @forelse ($hero_sections as $hero_section)
     <input type="hidden" name="path_video" id="path_video" value="{{ $hero_section->path_video }}">
     <div class="container max-w-[1130px] mx-auto relative pt-10 z-10">
-      <nav class="flex flex-wrap items-center justify-between bg-white p-[20px_30px] rounded-[20px] gap-y-3">
-          <div class="flex items-center gap-3">
-              <div class="flex shrink-0 h-[43px] overflow-hidden">
-                  <img src="{{ asset('assets/logo/logo.svg') }}" class="object-contain w-full h-full" alt="logo">
-              </div>
-              <div class="flex flex-col">
-                <p id="CompanyName" class="font-extrabold text-xl leading-[30px]">ShaynaComp</p>
-                <p id="CompanyTagline" class="text-sm text-cp-light-grey">Build Futuristic Dreams</p>
-              </div>
-          </div>
-          <ul class="flex flex-wrap items-center gap-[30px]">
-            <li class="font-semibold transition-all duration-300 hover:text-cp-dark-blue text-cp-dark-blue">
-              <a href="index.html">Home</a>
-            </li>
-            <li class="font-semibold transition-all duration-300 hover:text-cp-dark-blue">
-              <a href="">Products</a>
-            </li>
-            <li class="font-semibold transition-all duration-300 hover:text-cp-dark-blue">
-              <a href="">Company</a>
-            </li>
-            <li class="font-semibold transition-all duration-300 hover:text-cp-dark-blue">
-              <a href="">Blog</a>
-            </li>
-            <li class="font-semibold transition-all duration-300 hover:text-cp-dark-blue">
-              <a href="about.html">About</a>
-            </li>
-          </ul>
-          <a href="" class="bg-cp-dark-blue p-[14px_20px] w-fit rounded-xl hover:shadow-[0_12px_30px_0_#312ECB66] transition-all duration-300 font-bold text-white">Get a Quote</a>
-      </nav>
+      <x-navbar/>
       <div id="Hero" class="flex flex-col gap-[30px] mt-20 pb-20">
         <div class="flex items-center bg-white p-[8px_16px] gap-[10px] rounded-full w-fit">
           <div class="flex w-5 h-5 overflow-hidden shrink-0">
